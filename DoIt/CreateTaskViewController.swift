c2b51cb42c6ffbfc76590c558b3d6680ed1a3ca5
Mac <mac@Macs-iMac.local>
2017-05-26 19:34:51 -0500

    Working with Core Data!

--- DoIt/CreateTaskViewController.swift
+++ DoIt/CreateTaskViewController.swift
@@ -13,8 +13,6 @@ class CreateTaskViewController: UIViewController {
     @IBOutlet weak var taskNameText: UITextField!
     @IBOutlet weak var importantSwitch: UISwitch!
     
-    var previousVC = TasksViewController()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,13 +23,15 @@ class CreateTaskViewController: UIViewController {
     @IBAction func addTapped(_ sender: Any) {
         //Create a Task from the outlet information
         
-        let task = Task()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let task = Task(context: context)
         task.name = taskNameText.text!
         task.important = importantSwitch.isOn
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
-        //Add new task to array in previous ViewController
-        previousVC.tasks.append(task)
-        previousVC.tableView.reloadData()
+        //Pop back
+        
         navigationController!.popViewController(animated: true)
     }
 
